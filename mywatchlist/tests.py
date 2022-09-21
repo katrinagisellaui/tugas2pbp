eeb691be7fa81244da74fef89d5da06f860f5de0
Katrina <katrina.gisella@ui.ac.id>
2022-09-21 22:45:48 +0700

    ral reviews

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase, Client
+
+client = Client()
+
+
+def test_create(self):
+    """Test the url for "create"
+    """
+
+    response = self.client.get('/mywatchlist/html/{0}/'.format(self.userName))
+    self.assertEqual(response.status_code, 200)
